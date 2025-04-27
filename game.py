213f4e0e7606b69940a243b4752c34f518e18dec
moon-shine-coder <semenvasilev09072008@gmail.com>
2025-04-27 21:46:51 +0300

    начальная версия игры

---
+++ game.py
@@ -0,0 +1,14 @@
+from random import randint 
+
+secret_number = randint(1, 100)
+
+while secret_number != user_number:
+    user_number = int(input())
+    if user_number == secret_number:
+        print("You're guess!")
+    elif user_number > secret_number:
+        print("Your number is bigger then guess number.")
+    else:
+        print("Your number is lower then guess number.")
+        
+        
